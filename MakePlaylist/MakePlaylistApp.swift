80e9a80c1d5fd8e5ce8e470b9ba7fa9a55c95fb9
Serena Badesha <sbadesha@gmail.com>
2021-11-05 09:58:43 +0000

    Initial Commit

---
+++ MakePlaylist/MakePlaylistApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakePlaylistApp.swift
+//  MakePlaylist
+//
+//  Created by Serena Badesha on 05/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct MakePlaylistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
